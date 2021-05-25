b35d4c5e27a5f8f3b9a91924dddaf79eb8fb9c87
MINSK\wheercool <A.Rudy@sam-solutions.com>
2021-05-25 09:56:51 +0300

    fix

--- src/script.js
+++ src/script.js
@@ -1,98 +1,67 @@
 import './style.css'
 import * as THREE from 'three'
-import * as dat from 'dat.gui'
 
 import fragment from './fragment.glsl';
 import vertex from './vertex.glsl';
 
-// Debug
-const gui = new dat.GUI()
+var container;
+var camera, scene, renderer;
+var uniforms;
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+init();
+animate();
 
-// Scene
-const scene = new THREE.Scene()
+function init() {
+  container = document.getElementById('container');
 
-// Objects
-const geometry = new THREE.PlaneBufferGeometry(2, 2);
+  camera = new THREE.Camera();
+  camera.position.z = 1;
 
-const uniforms = {
-  u_time: {type: "f", value: 1.0},
-  u_resolution: {type: "v2", value: new THREE.Vector2()},
-  u_mouse: {type: "v2", value: new THREE.Vector2()}
-};
+  scene = new THREE.Scene();
 
-// Materials
+  const geometry = new THREE.PlaneBufferGeometry(2, 2);
 
-const material = new THREE.ShaderMaterial({
-  uniforms: uniforms,
-  vertexShader: vertex,
-  fragmentShader: fragment
-});
+  uniforms = {
+    u_time: {type: "f", value: 1.0},
+    u_resolution: {type: "v2", value: new THREE.Vector2()},
+    u_mouse: {type: "v2", value: new THREE.Vector2()}
+  };
 
-// Mesh
-const plane = new THREE.Mesh(geometry, material)
-scene.add(plane)
+  var material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: vertex,
+    fragmentShader: fragment
+  });
 
-/**
- * Sizes
- */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  onWindowResize();
+  window.addEventListener('resize', onWindowResize, false);
+
+  document.onmousemove = function (e) {
+    uniforms.u_mouse.value.x = e.pageX
+    uniforms.u_mouse.value.y = e.pageY
+  }
 }
 
-window.addEventListener('resize', () => {
-  // Update sizes
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
-
-  // Update camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
+function onWindowResize(event) {
+  renderer.setSize(window.innerWidth, window.innerHeight);
   uniforms.u_resolution.value.x = renderer.domElement.width;
   uniforms.u_resolution.value.y = renderer.domElement.height;
-})
-
-document.onmousemove = function (e) {
-  uniforms.u_mouse.value.x = e.pageX
-  uniforms.u_mouse.value.y = e.pageY
 }
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.Camera()
-camera.position.z = 1;
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+}
 
-scene.add(camera)
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-
-const tick = () => {
+function render() {
   uniforms.u_time.value += 0.05;
-  // Render
   renderer.render(scene, camera);
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
 }
-
-tick()
