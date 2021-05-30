b892784922a4b55e038125da829a6cf961c6d722
MINSK\wheercool <A.Rudy@sam-solutions.com>
2021-05-31 01:48:28 +0300

    filters: bright warm/cold grayscale
    stress

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,7 +1,29 @@
 uniform vec2 u_resolution;
 uniform float u_time;
+uniform sampler2D u_texture;
+uniform vec2 u_texture_uv;
+uniform vec3 u_mouse;
 
 void main() {
-    vec2 st = gl_FragCoord.xy/u_resolution.xy;
-    gl_FragColor=vec4(st.x,st.y,0.0,1.0);
+    vec2 pos = gl_FragCoord.xy / u_texture_uv;
+    vec4 color = texture2D(u_texture, pos);
+
+//    float warmness = 0.0; // +0.2 - wram   -0.2 -cold
+//    color.r += warmness;
+//    color.b -= warmness;
+//
+//    float brightness = 0.2;
+//    color.rgb += brightness;
+//
+//    float grayscale = (color.r + color.g + color.b) / 3.0;
+//    color.rbg = vec3(grayscale);
+
+    float shift = 100.0 * cos(u_time * 100.0) / pow(u_time, 2.0);
+    vec2 left_pos = (gl_FragCoord.xy + vec2(shift, 0.0)) / u_texture_uv;
+    vec2 top_pos = (gl_FragCoord.xy + vec2(0.0, 0.0)) / u_texture_uv;
+    vec4 left_color = texture2D(u_texture, left_pos);
+    vec4 top_color = texture2D(u_texture, top_pos);
+    color = 0.5 * (color + left_color);
+
+    gl_FragColor = color;
 }
--- src/script.js
+++ src/script.js
@@ -9,7 +9,6 @@ var camera, scene, renderer;
 var uniforms;
 
 init();
-animate();
 
 function init() {
   container = document.getElementById('container');
@@ -18,13 +17,21 @@ function init() {
   camera.position.z = 1;
 
   scene = new THREE.Scene();
+  const loader = new THREE.TextureLoader();
+  loader.load('./image.png', (texture) => {
+    uniforms.u_texture.value = texture;
+    uniforms.u_texture_uv.value = new THREE.Vector2(texture.image.width, texture.image.height);
+    animate();
+  })
 
   const geometry = new THREE.PlaneBufferGeometry(2, 2);
 
   uniforms = {
     u_time: {type: "f", value: 1.0},
     u_resolution: {type: "v2", value: new THREE.Vector2()},
-    u_mouse: {type: "v2", value: new THREE.Vector2()}
+    u_mouse: {type: "v2", value: new THREE.Vector3()},
+    u_texture: {value: new THREE.Texture()},
+    u_texture_uv: {value: new THREE.Vector2()}
   };
 
   var material = new THREE.ShaderMaterial({
@@ -43,7 +50,13 @@ function init() {
 
   onWindowResize();
   window.addEventListener('resize', onWindowResize, false);
+  window.addEventListener('mousedown', () => {
+    uniforms.u_mouse.value.z = 1;
+  }, false);
 
+  window.addEventListener('mouseup', () => {
+    uniforms.u_mouse.value.z = 0;
+  }, false);
   document.onmousemove = function (e) {
     uniforms.u_mouse.value.x = e.pageX
     uniforms.u_mouse.value.y = e.pageY
