44216092582f9230b44a6d07947ae8c9b1efab71
MINSK\wheercool <A.Rudy@sam-solutions.com>
2021-05-31 14:59:39 +0300

    Spinning color wheel

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# Three.js + GLSL Starter
+# The Book of shaders chapter 6. Colors
 
-## Setup
-Download [Node.js](https://nodejs.org/en/download/).
-Run this followed commands:
-
-``` bash
-# Install dependencies (only the first time)
-npm install
-
-# Run the local server at localhost:8080
-npm run dev
-
-# Build for production in the dist/ directory
-npm run build
-```
+Task: Modify the polar example to get a spinning color wheel, just like the waiting mouse icon
--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,7 +1,42 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define TWO_PI 6.28318530718
+
 uniform vec2 u_resolution;
 uniform float u_time;
 
-void main() {
-    vec2 st = gl_FragCoord.xy/u_resolution.xy;
-    gl_FragColor=vec4(st.x,st.y,0.0,1.0);
+//  Function from Iñigo Quiles
+//  https://www.shadertoy.com/view/MsS3Wc
+vec3 hsb2rgb(in vec3 c){
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0, 4.0, 2.0),
+    6.0)-3.0)-1.0,
+    0.0,
+    1.0);
+    rgb = rgb*rgb*(3.0-2.0*rgb);
+    return c.z * mix(vec3(1.0), rgb, c.y);
+}
+
+void main(){
+    vec2 st = gl_FragCoord.xy/u_resolution;
+    vec3 color = vec3(0.0);
+
+    // Use polar coordinates instead of cartesian
+    vec2 toCenter = vec2(0.5)-st;
+    float angle = atan(toCenter.y, toCenter.x);
+    // radius [0, 1]
+    float radius = length(toCenter)*2.0;
+
+    // Map the angle (-PI to PI) to the Hue (from 0 to 1)
+    // and the Saturation to the radius
+    float angle_normalized = angle/TWO_PI+0.5 + u_time / 3.0;
+    float R = 0.8 * (cos(u_time / 10.0) * 0.5 + 0.5);
+    radius = radius * 0.5 / (0.5 * R);
+
+    color = hsb2rgb(vec3(angle_normalized, radius, 1.0));
+
+    float circle = smoothstep(R, R + 0.001, radius);
+    color = mix(vec3(0.0), color, 1.0 - circle);
+    gl_FragColor = vec4(color, 1.0);
 }
